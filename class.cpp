ff7871c0eae4a7999b9b3dcc94ec56617e8ebbd9
ydicsey <67023584+ydicsey@users.noreply.github.com>
2023-04-07 17:22:28 +0000

    add Operator

--- class.cpp
+++ class.cpp
@@ -18,33 +18,31 @@ class Pos
 
 class Entity
 {
-private:
-    int id_;
-    int type_;
-    int x_;
-    int y_;
-    int shield_life_;
-    int is_controlled_;
-    int health_;
-    double operator-(const Entity &other)
-    {
-        int x = x_ - other.x_;
-        int y = y_ - other.y_;
-        x *= x;
-        y *= y;
-        return sqrt(x + y);
-    }
-public:
-    Entity(int id, int type, int x, int y, int shield_life, int is_controlled, int health);
-    const int get_ID() const { return id_; };
-    const int get_X() const { return x_; };
-    const int get_Y() const { return y_; };
-    const int get_ShieldLife() const { return shield_life_; };
-    const int get_IsControlled() const { return is_controlled_; };
-    const int get_Health() const { return health_; };
+    private:
+        int id_;
+        int type_;
+        int x_;
+        int y_;
+        int shield_life_;
+        int is_controlled_;
+        double operator-(const Entity &other)
+        {
+            int x = x_ - other.x_;
+            int y = y_ - other.y_;
+            x *= x;
+            y *= y;
+            return sqrt(x + y);
+        }
+    public:
+        Entity(int id, int type, int x, int y, int shield_life, int is_controlled);
+        const int get_ID() const { return id_; };
+        const int get_X() const { return x_; };
+        const int get_Y() const { return y_; };
+        const int get_ShieldLife() const { return shield_life_; };
+        const int get_IsControlled() const { return is_controlled_; };
 };
 
-Entity::Entity(int id, int type, int x, int y, int shield_life, int is_controlled, int health)
+Entity::Entity(int id, int type, int x, int y, int shield_life, int is_controlled)
 {
     id_ = id;
     type_ = type;
@@ -52,19 +50,44 @@ Entity::Entity(int id, int type, int x, int y, int shield_life, int is_controlle
     y_ = y;
     shield_life_ = shield_life;
     is_controlled_ = is_controlled;
-    health_ = health;
 }
 
 class Hero : public Entity
 {
     public:
-        Hero(int id, int type, int x, int y, int shield_life, int is_controlled, int health);
+        Hero(int id, int type, int x, int y, int shield_life, int is_controlled);
     private:
         // 不知道hero要不要其他的資訊
         // 比如：一開始的位置之類的
 };
 
-Hero::Hero(int id, int type, int x, int y, int shield_life, int is_controlled, int health) : Entity(id, type, x, y, shield_life, is_controlled, health)
+Hero::Hero(int id, int type, int x, int y, int shield_life, int is_controlled) : Entity(id, type, x, y, shield_life, is_controlled)
 {
 
 };
+
+class Monsters : public Entity
+{
+public:
+    Monsters(int id, int type, int x, int y, int shield_life, int is_controlled, int health, int vx, int vy, int nearBase, int threatFor);
+    const int get_Health() const { return health_; };
+    const int get_VX() const { return vx_; };
+    const int get_VY() const { return vy_; };
+    const int get_NearBase() const { return nearBase_; };
+    const int get_ThreatFor() const { return threatFor_; };
+private:
+    int health_;
+    int vx_;
+    int vy_;
+    int nearBase_;
+    int threatFor_;
+};
+
+Monsters::Monsters(int id, int type, int x, int y, int shield_life, int is_controlled, int health, int vx, int vy, int nearBase, int threatFor) : Entity(id, type, x, y, shield_life, is_controlled)
+{
+    health_ = health;
+    vx_ = vx;
+    vy_ = vy;
+    nearBase_ = nearBase;
+    threatFor_ = threatFor;
+}
