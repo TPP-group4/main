fc6fd90980d645b426f7eb016e80da63c9a01b31
ydicsey <67023584+ydicsey@users.noreply.github.com>
2023-04-13 17:53:30 +0000

    modify input: add my_monsters, enemy_monsters ...

--- class.cpp
+++ class.cpp
@@ -188,6 +188,18 @@ namespace Player{
         {
             // 1=your base, 2=your opponent's base, 0=neither
             Monsters tmp(id, type, x, y, shield_life, is_controlled, health, vx, vy, near_base, threat_for);       
+            if(near_base == 1)
+            {
+                my_monsters.emplace_back(tmp);
+            }
+            else if(near_base == 0)
+            {
+                enemy_monsters.emplace_back(tmp);
+            }
+            else if(near_base == 2)
+            {
+                neutral_monsters.emplace_back(tmp);
+            }
             monsters.emplace_back(tmp);
         }
     }
@@ -311,9 +323,42 @@ namespace Player{
         return;
     }
 
-
-    pair<int ,int > &windPos(){
-
+    // 輸入r
+    // 隨機回傳在1/4圓中的任意位置
+    // 圓心：mybase
+    pair<int, int> random_pos_circle(int range)
+    {
+        int base_top_left = my_Base.get_X() == 0 ? 1 : 0; 
+        int theta;
+        if(base_top_left)
+        {
+            int minValue = 0;
+            int maxValue = 90;
+            theta = rand() % (maxValue - minValue + 1) + minValue;
+        }
+        else
+        {
+            int minValue = 180;
+            int maxValue = 270;
+            theta = rand() % (maxValue - minValue + 1) + minValue;
+        }
+        auto pi = acos(-1.0);
+        // 將角度轉為弧度
+        double radian = theta * pi / 180.0;
+        // generate number between 1 and range
+        int r = rand() % range + 1;
+        int x = r * cos(radian);
+        int y = r * sin(radian);
+        pair<int, int> tmp;
+        if(base_top_left)
+        {
+            tmp = make_pair(x, y);
+        }
+        else
+        {
+            tmp = make_pair(17639 + x, 9000+ y);
+        }
+        return tmp;
     }
 };
 
